f3caa4505b9837a2c3f035147f1a4183855c85c6
ajgp8821 <ajgp8821@gmail.com>
2022-03-03 23:04:17 -0300

    Script NextLevel

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+    [SerializeField]
+    string levelToLoad;
+    [SerializeField]
+    float delay;
+
+    [ContextMenu("Activate Load")]
+    public void ActivateLoad() {
+        Invoke("LoadLevel", delay);
+    }
+
+    private void LoadLevel() {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public bool IsLastLevel() {
+        return levelToLoad == "Title";
+    }
+
+}
