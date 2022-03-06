83d1b0e96d8d8350f2303f238ee7ffd225302bd7
ajgp8821 <ajgp8821@gmail.com>
2022-03-06 05:12:54 -0300

    Upgrade BallController

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,9 +7,12 @@ public class BallController : MonoBehaviour {
     [SerializeField]
     float intialSpeed = 600f;
 
+    private float maxSpeed = 18f;
+    private float minSpeed = 4f;
     private Rigidbody rig;
     private Vector3 initialPosition;
     private bool isGame;
+    private bool isfirstCollision;
     private Transform bar;
 
     private void Awake() {
@@ -40,5 +43,26 @@ public class BallController : MonoBehaviour {
     public void StopMotion() {
         rig.isKinematic = true;
         rig.velocity = Vector3.zero;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("bar") && isfirstCollision) {
+            float contactPoint = collision.contacts[0].point.x;
+            float positionBarX = collision.gameObject.transform.position.x;
+            float colliderPoint = contactPoint - positionBarX;
+
+            float angle = colliderPoint * (Mathf.PI / 3);
+            float speedX = maxSpeed * Mathf.Sin(angle);
+            speedX = Mathf.Clamp(speedX, -maxSpeed, maxSpeed);
+            
+            float speedY = maxSpeed * Mathf.Cos(angle);
+            speedY = Mathf.Abs(speedY);
+            speedY = Mathf.Clamp(speedY, minSpeed, maxSpeed);
+
+            rig.velocity = new Vector3(speedX, speedY, 0);
+        }
+        else {
+            isfirstCollision = true;
+        }
     }
 }
--- Assets/Scripts/SoundsBall.cs
+++ Assets/Scripts/SoundsBall.cs
@@ -8,20 +8,15 @@ public class SoundsBall : MonoBehaviour {
     AudioSource ballBounce;
     [SerializeField]
     AudioSource point;
-    [SerializeField]
-    AudioSource loseLive;
 
     private void OnCollisionEnter(Collision collision) {
-        Debug.Log("collision " + collision.gameObject.tag);
+        // Debug.Log("collision " + collision.gameObject.tag);
 
         if (collision.gameObject.CompareTag("brick")) {
             point.Play();
         }
         else if (collision.gameObject.CompareTag("bar") || collision.gameObject.CompareTag("column")) {
             ballBounce.Play();
-        }
-        else if (collision.gameObject.CompareTag("ground")) {
-            loseLive.Play();
         }
     }
 }
